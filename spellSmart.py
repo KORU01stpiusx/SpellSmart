39fd905f1ec6116aafbfc7f442035086830c92af
Callum Koruga <koru01@stpiusx.nsw.edu.au>
2025-10-21 09:02:04 +1100

    Start of 21/10 lesson

--- spellSmart.py
+++ spellSmart.py
@@ -1,10 +1,10 @@
-import pygame
+#import pyqt6
 import sys
+from random import randint
 
-
-from models.Person import Person
-from models.Word import Word
-from models.Result import Result
+# from models.Person import Person
+#from models.Word import Word
+# from models.Result import Result
 
 from utilities.Avatar import Avatar
 from utilities.Choice import Choice
@@ -12,21 +12,36 @@ from utilities.NLP import NLP
 
 class SpellSmart(Avatar):
 
+    def __init__(self, name="Broomhilda", userSR=True, show=True):
 
-    def __init__(self, name="Broomhilda", userSR=True, show=True, vix=0):
-
-        super().__init__(name, userSR, show, vix)
+        super().__init__(name, userSR, show)
         self.say(f"Hello! Welcome to spell smart! My name is {self.getName()}. I will help you practice your spelling skills.")
 
-    def spellWord(self,word=None):
+    def getWord(self):
+        words = ["banana", "apple", "grape", "orange", "melon", "pineapple"]
+        word = words[randint(0, len(words) - 1)]
+        self.say(f"Can you spell {word}",show = False)
+        answer = input('Please spell the word here: ')
+        self.spellWord(word, answer)
+       
+   
+   
+   
+    def spellWord(self, word, answer=None):
         self.say(f"You spelt the word: {word} as follows:")
-        for letter in word:
-            self.say(letter)
+        rightLetters = 0
+        for letter in answer:
+            self.say(letter.upper())
+        for i in range(len(answer)):
+            if word[i].upper() == answer[i].upper():
+                rightLetters +=1
+        self.say(f"You got {rightLetters} out of {len(word)} letters correct")
 
-    def test():
-        game = SpellSmart()
-        game.spellWord("elephant")
+   
 
-
-    if __name__ == "__main__":
-        test()
+def test():
+    game = SpellSmart()
+    game.getWord()
+   
+if __name__ == "__main__":
+    test()
